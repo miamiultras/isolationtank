a10948d80a117659fc86f8bc542076f09c2e7691
eeeyyy <26793642+miamiultras@users.noreply.github.com>
2023-07-10 16:34:03 +0200

    refactor Header, clean up global styles

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -12,9 +12,6 @@
 	</div>
 
 	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
 		<ul>
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
@@ -26,9 +23,6 @@
 				<a href="/sverdle">Sverdle</a>
 			</li>
 		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
 	</nav>
 
 	<div class="corner">
@@ -42,6 +36,10 @@
 	header {
 		display: flex;
 		justify-content: space-between;
+		background: var(--color-bg-0);
+		background-size: contain;
+		padding: 0 2em;
+		border-bottom: 1px solid rgb(66, 66, 66);
 	}
 
 	.corner {
@@ -65,21 +63,12 @@
 
 	nav {
 		display: flex;
+		width: 100%;
 		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
 	}
 
 	ul {
+		width: 100%;
 		position: relative;
 		padding: 0;
 		margin: 0;
@@ -88,8 +77,6 @@
 		justify-content: center;
 		align-items: center;
 		list-style: none;
-		background: var(--background);
-		background-size: contain;
 	}
 
 	li {
@@ -97,16 +84,8 @@
 		height: 100%;
 	}
 
-	li[aria-current='page']::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
+	li[aria-current='page'] a {
+		border-bottom: 1px solid red;
 	}
 
 	nav a {
